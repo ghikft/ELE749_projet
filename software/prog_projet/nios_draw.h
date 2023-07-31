49eb0c299c52634e1cd19f814aa009716cecd053
Robin Galipeau <robingalipeau@outlook.com>
2023-07-31 16:32:02 -0400

    ajout fichier + déplacement fonction du main
    -ajout niosdraw.c et niosdraw.h
    -deplacement des fonction de niosDraw, du main vers les fichier nios_draw .c et .h

---
+++ software/prog_projet/nios_draw.h
@@ -0,0 +1,41 @@
+
+#ifndef NIOS_DRAW_H_
+#define NIOS_DRAW_H_
+#include "project_variables.h"
+#include "softwareDraw.h"
+
+
+void draw_icon(tool icon, char selected,
+	lastDrawingVar* lastDrawingData, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void tool_selection(Cursor* currentCursor, tool* currentTool, int startUsingTool,
+	int* selectedColor, char* left_btn, lastDrawingVar* lastDrawingData,
+	alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void draw_tool_bar(tool currentTool, lastDrawingVar* lastDrawingData, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void draw_icon_array(int start_x, int start_y, int row, int col, int* image, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void draw_selection_Frame(int x1, int y1, int x2, int y2, char selected,
+	lastDrawingVar* lastDrawingData, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void draw_color_palette(int selectedColor, lastDrawingVar* lastDrawingData, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void cursor_draw_sprite(Cursor* coordinate, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void cursor_erase(Cursor* coordinate, alt_u8* cursorMem, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void cursor_save(Cursor* coordinate, alt_u8* cursorMem);
+
+unsigned char get_pixel_color(int x, int y);
+
+void cursor_draw(char startUsingTool, Cursor* currentCursor, alt_u8* cursorMem, int* x_pos, int* y_pos, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void start_button(tool currentTool, char* startButtonPressed, unsigned char* left_btn, unsigned char* lastLeft, alt_u8* cursorMem,
+	Cursor* currentCursor, lastDrawingVar* lastDrawingData, alt_up_pixel_buffer_dma_dev* pixel_buffer);
+
+void process_cursor_pos(Cursor* currentCursor, int* x_pos, int* y_pos);
+
+
+
+#endif /* NIOS_DRAW_H_ */
--- software/prog_projet/project_variables.h
+++ software/prog_projet/project_variables.h
@@ -34,5 +34,61 @@
 #define BOTTOM_LIMIT 479
 #define DRAWING_ZONE_LEFT_LIMIT 61
 
+#define SCALE_FACTOR 0.1
+#define SCALE_FACTOR_INV 10
+//Colors
+#define BACKGROUD_COLOR WHITE//128//0xEB
+#define DRAW_COLOR 0
+#define CURSOR_COLOR BLACK
+#define TOOL_BOX_BACKGROUND_COLOR 0b11011111
+#define SELECTION_COLOR 128
 
+//#define BLACK 0
+#define ERASE_PREVIOUS_WORK 1
+#define NOT_ERASE_PREVIOUS_WORK 0
+
+#define ICON_LEFT_X 2
+#define ICON_TOP_Y 2
+#define ICON_WIDTH 26
+#define ICON_HEIGHT 26
+#define ICON_SPACING 2
+
+#define IN_TOOL_BAR 1
+#define NOT_IN_TOOL_BAR 0
+
+#define TOOL_SELECTED 1
+#define TOOL_NOT_SELECTED 0
+
+typedef enum mode {
+	DRAWING_MODE,
+	SNAKE_MODE,
+	PONG_MODE
+}mode;
+typedef enum tool {
+	NO_TOOL,
+	PENCIL,
+	EMPTY_RECTANGLE,
+	FILLED_RECTANGLE,
+	EMPTY_ELLIPSE,
+	FILLED_ELLIPSE,
+	LINE,
+	CPY_PASTE,
+	FILL,
+	CUT_PASTE,
+	COLOR_SAMPLE,
+	PONG,
+	SNAKE,
+	COLOR_SELECTION,
+	CLEAR
+}tool;
+
+typedef struct Cursor {
+	int x;
+	int y;
+} Cursor;
+
+typedef struct Point {
+	int x;
+	int y;
+} Point;
 #endif  // PROJECT_VARIABLES_H
